708a83d5ca5e43630a937a0d52cd84db8f8c2879
brandonmoak <developer@bmoak.com>
2025-01-31 22:06:18 -0500

    Map updates (#3)
    * map_updates
    
    * adding openseamap layer to visualize markers
    
    * updated boat icon to be more visible
    
    * updated application name

--- frontend/src/components/Boat.tsx
+++ frontend/src/components/Boat.tsx
@@ -1,23 +1,41 @@
 import L from 'leaflet';
 import { BoatState } from '../types';
 
-export const createBoatIcon = (heading: number): L.DivIcon => {
+// Add this type declaration
+declare module 'leaflet' {
+  interface MarkerOptions {
+    rotationAngle?: number;
+    rotationOrigin?: string;
+  }
+}
+
+interface BoatMarkerOptions extends BoatState {
+  iconSize?: [number, number];
+}
+
+export const createBoatIcon = (rotation: number, size: [number, number] = [32, 32]) => {
   return L.divIcon({
-    html: `<div style="transform: rotate(${heading}deg);">
+    html: `<div style="transform: rotate(${rotation}deg);">
             <img 
               src="/boat_favicon.png" 
               alt="boat" 
-              style="width: 24px; height: 24px; display: block;"
+              style="width: ${size[0]}px; height: ${size[1]}px; display: block; 
+                     filter: contrast(120%) brightness(90%);
+                     -webkit-filter: contrast(120%) brightness(90%);"
             />
           </div>`,
     className: 'boat-icon',
-    iconSize: [24, 24],
-    iconAnchor: [12, 12],
+    iconSize: size,
+    iconAnchor: [size[0]/2, size[1]/2],
   });
 };
 
-export const createBoatMarker = (position: BoatState): L.Marker => {
-  return L.marker([position.lat, position.lon], {
-    icon: createBoatIcon(position.heading),
+export const createBoatMarker = (options: BoatMarkerOptions) => {
+  const size = options.iconSize || [16, 16];
+  
+  return L.marker([options.lat, options.lon], {
+    icon: createBoatIcon(options.heading, size),
+    rotationAngle: options.heading,
+    rotationOrigin: 'center center'
   });
 }; 
