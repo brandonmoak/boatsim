6f5ca1dae4bd3e292534d409ecac89ba6721693e
Brandon Moak <developer@bmoak.com>
2025-02-05 01:33:16 -0500

    first package - fails on runtime

---
+++ frontend/babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+//   presets: ['react-app'],
+//   plugins: [
+//     [
+//       '@babel/plugin-transform-runtime',
+//       {
+//         useESModules: false, // This should force CommonJS helpers.
+//         // You might try adding `absoluteRuntime: false` explicitly if available.
+//       }
+//     ]
+//   ]
+}; 
--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,6 +1,7 @@
 const path = require('path');
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 
-module.exports = function override(config, env) {
+function override(config, env) {
     // Add shared folder alias
     config.resolve.alias = {
         ...config.resolve.alias,
@@ -14,5 +15,41 @@ module.exports = function override(config, env) {
         type: 'javascript/auto'
     });
 
+    // // Remove ModuleScopePlugin which restricts imports from outside src.
+    // config.resolve.plugins = config.resolve.plugins.filter(
+    //     (plugin) => !(plugin instanceof ModuleScopePlugin)
+    // );
+
+    // // Disable symlink resolution so that hoisted dependencies remain virtually in the workspace.
+    // config.resolve.symlinks = false;
+
+    // // Tell webpack to also look in the root's node_modules.
+    // config.resolve.modules = [
+    //     path.resolve(__dirname, "../node_modules"),
+    //     "node_modules",
+    // ];
+
+    // // Alias for the hoisted @babel/runtime package.
+    // config.resolve.alias["@babel/runtime"] = path.resolve(
+    //     __dirname,
+    //     "../node_modules/@babel/runtime"
+    // );
+    // // Add a specific alias for the helpers directory.
+    // config.resolve.alias["@babel/runtime/helpers/esm"] = path.resolve(
+    //     __dirname,
+    //     "../node_modules/@babel/runtime/helpers/esm"
+    // );
+
+    // Debug: Log key parts of the resolve configuration.
+    console.log("Webpack resolve.modules:", config.resolve.modules);
+    console.log("Webpack resolve.alias:", config.resolve.alias);
+    console.log("Webpack resolve.symlinks:", config.resolve.symlinks);
+    console.log(
+        "Webpack resolve.plugins:",
+        config.resolve.plugins.map((p) => p.constructor.name)
+    );
+
     return config;
-}; 
+}
+
+module.exports = override; 
